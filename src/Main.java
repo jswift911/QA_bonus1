7f74226cb60f167f6e083e029fc3ff0d9b45c634
Alex <jswift888@gmail.com>
2023-02-22 11:10:32 +0300

    task bonus1

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        //Стоимость билета
+        int ticket_price = 13676;
+        //Количество рублей для одной бонусной мили
+        int bonus_price = 20;
+
+        int bonus_count = (ticket_price / bonus_price);
+
+        System.out.println("Вым начислен бонус в " + bonus_count + " миль.");
+    }
+}
